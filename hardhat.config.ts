c795e18ac3a19f557023e4e350e3f4857d9c9706
june29 <june29.jp@gmail.com>
2022-12-31 13:24:45 +0900

    Prepare to deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_API_HTTPS_ENDPOINT_GOERLI,
+      accounts: [process.env.ETHEREUM_WALLET_PRIVATE_KEY]
+    },
+    mainnet: {
+      url: process.env.ALCHEMY_API_HTTPS_ENDPOINT_MAINNET,
+      accounts: [process.env.ETHEREUM_WALLET_PRIVATE_KEY]
+    }
+  }
 };
 
 export default config;
