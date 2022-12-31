4bdd33f1f526d9370234ae55a14ad7561bbd8b12
june29 <june29.jp@gmail.com>
2022-12-31 21:58:30 +0900

    Use Base64.sol

--- contracts/PochiBukuro.sol
+++ contracts/PochiBukuro.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract PochiBukuro is ERC721, Ownable {
   using Counters for Counters.Counter;
